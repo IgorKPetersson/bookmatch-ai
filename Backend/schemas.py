126805a489eabcc4067c6070faeedf5c9dd9f36c
Igor Petersson <igor.petersson@gmail.com>
2026-03-02 17:42:36 +0100

    Week 2: Full CRUD for books implemented

--- Backend/schemas.py
+++ Backend/schemas.py
@@ -8,3 +8,16 @@ class BookCreate(BaseModel):
     isbn: str
     release_year: int
     genre: str
+
+
+class BookRead(BaseModel):
+    id: int
+    title: str
+    authors: str
+    short_description: str
+    isbn: str
+    release_year: int
+    genre: str
+
+    class Config:
+        from_attributes = True
