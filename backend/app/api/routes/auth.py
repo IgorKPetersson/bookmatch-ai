f26761b14bb141de4a2a493c95cca35b16bd99b4
Oliver Cupan <oliver.cupan6@gmail.com>
2026-03-06 19:36:50 +0100

    Created a login endpoint and schema.

--- backend/app/api/routes/auth.py
+++ backend/app/api/routes/auth.py
@@ -1,8 +1,12 @@
+from datetime import datetime, timedelta, timezone
+
 import bcrypt
+from app.core.config import JWT_ALGORITHM, VERIFICATION_KEY
 from app.db.database import get_db
 from app.models.users import User
-from app.schemas.users import UserCreate
+from app.schemas.users import UserCreate, UserLogin
 from fastapi import APIRouter, Depends, HTTPException
+from jose import jwt
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -26,3 +30,26 @@ async def register(user_data: UserCreate, db: AsyncSession = Depends(get_db)):
     await db.commit()
 
     return {"message": "User is successfully registerd."}
+
+
+@router.post("/login")
+async def login(user_data: UserLogin, db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(User).where(User.email == user_data.email))
+    verfied_user = result.scalar_one_or_none()
+
+    if not verfied_user:
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+
+    password_check = bcrypt.checkpw(
+        user_data.password.encode("utf-8"), verfied_user.password_hash.encode("utf-8")
+    )
+
+    if not password_check:
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+    token_data = {
+        "sub": verfied_user.id,
+        "exp": datetime.now(timezone.utc) + timedelta(hours=24),
+    }
+    token = jwt.encode(token_data, VERIFICATION_KEY, algorithm=JWT_ALGORITHM)
+
+    return {"access_token": token, "token_type": "bearer"}
--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -7,3 +7,6 @@ load_dotenv()
 API_KEY = os.getenv("GOOGLE_BOOKS_API_KEY")
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+JWT_ALGORITHM = os.getenv("JWT_ALGORITHM", "HS256")
+VERIFICATION_KEY = os.getenv("VERIFICATION_KEY")
